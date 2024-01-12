1bae40bbe6abff7e72871e5b39c2bd7e336750f1
Attic <you@example.com>
2024-01-12 19:57:22 +0000

    Add scaffolding

--- ccse-cw1/Program.cs
+++ ccse-cw1/Program.cs
@@ -1,5 +1,6 @@
 using ccse_cw1.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
