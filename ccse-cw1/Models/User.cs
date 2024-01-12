3b82171d128972fb2252503d6b3594aaab0ca1ce
Attic <you@example.com>
2024-01-12 18:39:49 +0000

    Added models

---
+++ ccse-cw1/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ccse_cw1.Models
+{
+	public class User
+	{
+		[Required]
+		public int Id { get; set; }
+
+
+		[Required]
+		public string FullName { get; set; }
+
+		public string? Address { get; set; }
+
+		[Required]
+		public string Email { get; set; }
+
+		[Required]
+		public string PhoneNo { get; set; }
+
+		public string CustomerNo { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		public DateTime RegistrationDate { get; set; }
+	}
+}
