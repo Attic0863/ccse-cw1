0f76c326bd56d1afb596d06ea4421caaa8da8239
Attic <you@example.com>
2024-01-12 18:52:26 +0000

    Add dbcontext

---
+++ ccse-cw1/Services/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ccse_cw1.Services
+{
+	public class AppDbContext : DbContext
+	{
+        public AppDbContext(DbContextOptions options) : base(options)
+		{
+            
+        }
+    }
+}
