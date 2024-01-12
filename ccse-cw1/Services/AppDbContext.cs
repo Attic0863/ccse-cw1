c20f4a74677a4db645c6378c88becad84c317e3a
Attic <you@example.com>
2024-01-12 19:39:11 +0000

    add users

--- ccse-cw1/Services/AppDbContext.cs
+++ ccse-cw1/Services/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ccse_cw1.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ccse_cw1.Services
 {
-	public class AppDbContext : IdentityDbContext
+	public class AppDbContext : IdentityDbContext<User>
 	{
         public AppDbContext(DbContextOptions options) : base(options)
 		{
