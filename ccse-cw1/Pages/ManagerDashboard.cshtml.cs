302a8002d90068fa5d6ef5249aa17825a71f0bc0
Attic <you@example.com>
2024-01-12 23:44:35 +0000

    Create administrive dashboards

---
+++ ccse-cw1/Pages/ManagerDashboard.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ccse_cw1.Pages
+{
+	[Authorize(Roles = "manager")]
+	public class ManagerDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
