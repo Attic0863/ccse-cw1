7b096f6e3d2a3b9f604a335d5649cbcf348621b5
Attic <you@example.com>
2024-01-13 00:47:42 +0000

    Setup dashboard locations

--- ccse-cw1/Pages/Admin/Panel.cshtml.cs
+++ ccse-cw1/Pages/Admin/Panel.cshtml.cs
@@ -2,10 +2,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ccse_cw1.Pages
+namespace ccse_cw1.Pages.Admin
 {
-	[Authorize(Roles = "manager,admin")]
-	public class ManagerDashboardModel : PageModel
+    [Authorize(Roles = "admin")]
+    public class Panel : PageModel
     {
         public void OnGet()
         {
