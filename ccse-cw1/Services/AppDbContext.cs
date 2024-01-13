47538208595349ffee97d5230259ada4544c2fda
Attic <you@example.com>
2024-01-13 23:03:37 +0000

    Add relationship definitions

--- ccse-cw1/Services/AppDbContext.cs
+++ ccse-cw1/Services/AppDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace ccse_cw1.Services
 {
@@ -14,6 +15,8 @@ namespace ccse_cw1.Services
 		protected override void OnModelCreating(ModelBuilder builder)
 		{
 			base.OnModelCreating(builder);
+
+			// Define identity roles
 
 			var admin = new IdentityRole("admin")
 			{
@@ -31,6 +34,20 @@ namespace ccse_cw1.Services
 			};
 
 			builder.Entity<IdentityRole>().HasData(admin, customer, manager);
-		}
+
+            // Define relationships between the different entities, many to one, etc
+            builder.Entity<User>()
+                .HasMany(u => u.Bookings)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.UserId);
+
+            builder.Entity<Booking>()
+                .HasMany(b => b.RoomBookings)
+                .WithOne(rb => rb.Booking)
+                .HasForeignKey(rb => rb.BookingId);
+
+         // TODO: possible add cascading deleting
+
+        }
 	}
 }
