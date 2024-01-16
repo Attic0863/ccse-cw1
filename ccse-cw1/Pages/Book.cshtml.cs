5247480e763f94dc63f6073e2b4b2335a5184028
Attic <you@example.com>
2024-01-16 23:21:46 +0000

    Improve on booking page

--- ccse-cw1/Pages/Book.cshtml.cs
+++ ccse-cw1/Pages/Book.cshtml.cs
@@ -5,28 +5,35 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ccse_cw1.Pages
 {
     [Authorize]
     public class BookModel : PageModel
     {
-        public List<Hotel>? Hotels { get; set; }
-
         public BookModel()
         {
         }
 
-
+        [Required]
         [BindProperty]
-        public InputModel? Input { get; set; }
+        public required InputModel Input { get; set; }
 
         public class InputModel
         {
-            public required Booking booking { get; set; }
+            public required DateTime CheckInDate { get; set; }
+
+            public required DateTime CheckOutDate { get; set; }
+
+            public required int HotelId { get; set; }
+
+            public required string RoomType { get; set; }
+
+            public required int Amount { get; set; }
         }
 
-        public async Task OnGetAsync()
+        public void OnPost()
         {
 
 
