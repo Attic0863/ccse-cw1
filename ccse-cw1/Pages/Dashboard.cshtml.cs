e7c42feb18abd9c066b37e93b7020d9a703f84ad
Attic <you@example.com>
2024-01-16 00:42:11 +0000

    Added base dashboard

--- ccse-cw1/Pages/Dashboard.cshtml.cs
+++ ccse-cw1/Pages/Dashboard.cshtml.cs
@@ -4,33 +4,95 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Runtime.Intrinsics.X86;
 
 namespace ccse_cw1.Pages
 {
     [Authorize]
     public class DashboardModel : PageModel
     {
-		private readonly UserManager<ApplicationUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly UserRepository _userRepository;
         private readonly BookingRepository _bookingRepository;
 
         public ICollection<Booking>? bookings;
 
-		public DashboardModel(UserManager<ApplicationUser> userManager, UserRepository userRepository, BookingRepository bookingRepository)
+        public DashboardModel(UserManager<ApplicationUser> userManager, UserRepository userRepository, BookingRepository bookingRepository)
         {
             _userManager = userManager;
             _userRepository = userRepository;
             _bookingRepository = bookingRepository;
         }
-
         public void OnGet()
         {
-            var task = _userManager.GetUserAsync(User);
-            task.Wait();
+            // todo: get bookings from user or booking repo
+        }
 
-            var user = task.Result;
-            if (user != null)
-                bookings = user.Bookings;
+        public DateTime GenerateFirstCheckin(Booking booking)
+        {
+            // go through all types of bookings
+            var roomBookingFirstCheckin = booking.RoomBookings?
+                .OrderBy(rb => rb.CheckInDate)
+                .FirstOrDefault()?.CheckInDate;
+
+            var tourBookingFirstCheckin = booking.TourBooking?.CheckInDate;
+
+            // combine both check ins and then find the frst
+            var firstCheckinTime = new List<DateTime?> { roomBookingFirstCheckin, tourBookingFirstCheckin }
+                .Where(checkInTime => checkInTime.HasValue)
+                .Min();
+
+            return firstCheckinTime ?? DateTime.Now;
+        }
+
+        public DateTime GenerateLastCheckout(Booking booking)
+        {
+            // go through all types of bookings
+            var roomBookingLastCheckOut = booking.RoomBookings?
+                .OrderByDescending(rb => rb.CheckOutDate)
+                .FirstOrDefault()?.CheckOutDate;
+
+            var tourBookingLastCheckOut = booking.TourBooking?.CheckOutDate;
+
+            // combine both check outs and then find the latest
+            var lastCheckOutTime = new List<DateTime?> { roomBookingLastCheckOut, tourBookingLastCheckOut }
+                .Where(checkOutTime => checkOutTime.HasValue)
+                .Max();
+
+            return lastCheckOutTime ?? DateTime.Now;
+        }
+
+        public string GenerateHumanReadableTitle(Booking booking)
+        {
+            if (booking.RoomBookings != null && booking.RoomBookings.Any())
+            {
+                var firstRoomBooking = booking.RoomBookings.First();
+                // use roomtype if only 1 booked
+                if (booking.RoomBookings.Count == 1 && firstRoomBooking.Room != null)
+                {
+                    if (firstRoomBooking.Room.Hotel != null)
+                        return $"{firstRoomBooking.Room.Hotel.Name} - {firstRoomBooking.Room.RoomType}";
+                }
+                else
+                {
+                    // if there is more than 1 room booked
+                    if (booking.RoomBookings.Count > 1 && firstRoomBooking.Room != null)
+                    {
+                        if (firstRoomBooking.Room.Hotel != null)
+                            return $"{firstRoomBooking.Room.Hotel.Name} - {booking.RoomBookings.Count} Rooms";
+                    }
+                }
+            }
+            else  // use tour booking
+            {
+                if (booking.TourBooking != null && (booking.RoomBookings == null || booking.RoomBookings.Count <= 0))
+                {
+                    return $"{booking.TourBooking.Tour.Name} - {booking.TourBooking.Tour.Duration}";
+                }
+            }
+
+            // no human readable way
+            return "Multiple Bookings";
         }
     }
 }
