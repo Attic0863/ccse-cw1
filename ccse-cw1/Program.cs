797cb0beae888cf543e6bfcaaa9609f3e40f9adc
Attic <you@example.com>
2024-01-16 21:31:34 +0000

    Configured json serialisation for controllers

--- ccse-cw1/Program.cs
+++ ccse-cw1/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using ccse_cw1.Models;
 using ccse_cw1.Repositories;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,13 +12,17 @@ builder.Services.AddRazorPages();
 
 // add repositories to services
 builder.Services.AddTransient<UserRepository>();
-builder.Services.AddTransient<BookingRepository>();
-builder.Services.AddTransient<HotelRepository>();
+//builder.Services.AddTransient<BookingRepository>();
+//builder.Services.AddTransient<HotelRepository>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddRoles<IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{ // ignore cycles of data when returning data (useful for reducing navigator load)
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
 var app = builder.Build();
 
@@ -36,6 +41,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// map pages and api controllers
+app.MapControllers();
 app.MapRazorPages();
 
 app.Run();
