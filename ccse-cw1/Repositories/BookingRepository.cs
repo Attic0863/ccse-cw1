31d299873443014cff033577b7c268c5c95517a1
Attic <you@example.com>
2024-01-17 21:31:55 +0000

    Add bookingrepository

---
+++ ccse-cw1/Repositories/BookingRepository.cs
@@ -0,0 +1,73 @@
+﻿using ccse_cw1.Controllers;
+using ccse_cw1.Models;
+using ccse_cw1.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace ccse_cw1.Repositories
+{
+    public class BookingRepository
+    {
+        private readonly AppDbContext _context;
+        public BookingRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        private async Task<List<Room>> GetAvailableRoomsAsync(int hotelId, string roomtype, DateTime startDate, DateTime endDate, int amount = 1)
+        {
+            var AvailableRooms = new List<Room>();
+
+            var hotel = await _context.Hotels.Include(h => h.Rooms).FirstOrDefaultAsync(h => h.Id == hotelId);
+            var hotelRooms = hotel.Rooms;
+
+            foreach (var room in hotelRooms)
+            {
+                if (room.RoomType == roomtype && room.IsAvailable(startDate, endDate))
+                {
+                    AvailableRooms.Add(room);
+                }
+            }
+
+            return AvailableRooms;
+        }
+
+        public async Task<Booking> CreateRoomBooking(string userId, DateTime checkindate, DateTime checkoutdate, string roomtype, int hotelid)
+        {
+            var booking = new Booking
+            {
+                UserId = "-1",
+                BookingDate = DateTime.Now,
+
+            };
+
+            var availableRooms = await GetAvailableRoomsAsync(hotelid, roomtype, checkindate, checkoutdate);
+            if (availableRooms != null)
+            {
+                booking.UserId = userId;
+
+                var roombooking = new Room_Booking
+                {
+                    CheckInDate = checkindate,
+                    CheckOutDate = checkoutdate,
+                    Booking = booking,
+                    RoomId = availableRooms.FirstOrDefault().Id,
+                };
+
+                _context.Booking.Add(booking);
+                _context.RoomBooking.Add(roombooking);
+
+                await _context.SaveChangesAsync();
+
+                return booking;
+            }
+
+            return booking;
+
+        }
+           
+
+        private async Task<List<Booking>> CreateMultipleBooking(string userId, List<Booking> bookings)
+        {
+            return new List<Booking>();
+        }
+    }
+}
