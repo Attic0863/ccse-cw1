b24dac21cd5abc4805c7f73b7f170febbca8d52d
Attic <you@example.com>
2024-01-19 22:03:22 +0000

    Improved management panel (added searching)

--- ccse-cw1/Pages/Management/Panel.cshtml.cs
+++ ccse-cw1/Pages/Management/Panel.cshtml.cs
@@ -1,3 +1,5 @@
+using ccse_cw1.Models;
+using ccse_cw1.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -7,8 +9,22 @@ namespace ccse_cw1.Pages.Management
     [Authorize(Roles = "manager,admin")]
     public class ManagerDashboardModel : PageModel
     {
+        public List<Tour>? Tours { get; set; }
+        public List<Hotel>? HotelList { get; set; }
+        public List<Booking>? Bookings { get; set; }
+
+
+        private readonly BookingRepository _bookingRepository;
+        public ManagerDashboardModel(BookingRepository bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+
         public void OnGet()
         {
+            Bookings = _bookingRepository.GetBookings();
+            HotelList = _bookingRepository.GetHotels();
+            Tours = _bookingRepository.GetTours();
         }
     }
 }
